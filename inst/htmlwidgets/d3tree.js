a1267906c1c617e97b563401408419395bbdae84
timelyportfolio <kent.russell@timelyportfolio.com>
2015-07-17 11:35:40 -0500

    go with svg text labels instead of foreignObject

--- inst/htmlwidgets/d3tree.js
+++ inst/htmlwidgets/d3tree.js
@@ -6,16 +6,23 @@ HTMLWidgets.widget({
 
   initialize: function(el, width, height) {
 
-    return {  }
+    return {
+
+    }
 
   },
 
   renderValue: function(el, x, instance) {
 
     // thanks Bill White for his treemap code on which this is based
+    //    http://www.billdwhite.com/wordpress/2012/12/16/d3-treemap-with-title-headers/
 
-    var chartWidth = 550;
-    var chartHeight = 550;
+    // set this as constant false instead of trying to detect browser type
+    //   but leave in case we need to revert back to detecting IE
+    var isIE = false;
+
+    var chartWidth = el.getBoundingClientRect().width;
+    var chartHeight = el.getBoundingClientRect().height;
     var xscale = d3.scale.linear().range([0, chartWidth]);
     var yscale = d3.scale.linear().range([0, chartHeight]);
     var color = d3.scale.category10();
@@ -71,154 +78,166 @@ HTMLWidgets.widget({
         .attr("in2", "blurOut")
         .attr("mode", "normal");
 
+    drawTree( x.data );
 
-    node = root = x.data;
-    var nodes = treemap.nodes(root);
+    function drawTree(data) {
+        node = root = data;
+        var nodes = treemap.nodes(root);
 
-    var children = nodes.filter(function(d) {
-        return !d.children;
-    });
-    var parents = nodes.filter(function(d) {
-        return d.children;
-    });
-
-    // create parent cells
-    var parentCells = chart.selectAll("g.cell.parent")
-        .data(parents, function(d) {
-            return "p-" + d.id;
+        var children = nodes.filter(function(d) {
+            return !d.children;
         });
-    var parentEnterTransition = parentCells.enter()
-        .append("g")
-        .attr("class", "cell parent")
-        .on("click", function(d) {
-            zoom(d);
-        });
-    parentEnterTransition.append("rect")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", headerHeight)
-        .style("fill", headerColor);
-    parentEnterTransition.append('foreignObject')
-        .attr("class", "foreignObject")
-        .append("xhtml:body")
-        .attr("class", "labelbody")
-        .append("div")
-        .attr("class", "label");
-    // update transition
-    var parentUpdateTransition = parentCells.transition().duration(transitionDuration);
-    parentUpdateTransition.select(".cell")
-        .attr("transform", function(d) {
-            return "translate(" + d.dx + "," + d.y + ")";
-        });
-    parentUpdateTransition.select("rect")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", headerHeight)
-        .style("fill", headerColor);
-    parentUpdateTransition.select(".foreignObject")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", headerHeight)
-        .select(".labelbody .label")
-        .text(function(d) {
-            return d.name;
-        });
-    // remove transition
-    parentCells.exit()
-        .remove();
-
-    // create children cells
-    var childrenCells = chart.selectAll("g.cell.child")
-        .data(children, function(d) {
-            return "c-" + d.id;
-        });
-    // enter transition
-    var childEnterTransition = childrenCells.enter()
-        .append("g")
-        .attr("class", "cell child")
-        .on("click", function(d) {
-            zoom(node === d.parent ? root : d.parent);
-        })
-        .on("mouseover", function() {
-            this.parentNode.appendChild(this); // workaround for bringing elements to the front (ie z-index)
-            d3.select(this)
-                .attr("filter", "url(#outerDropShadow)")
-                .select(".background")
-                .style("stroke", "#000000");
-        })
-        .on("mouseout", function() {
-            d3.select(this)
-                .attr("filter", "")
-                .select(".background")
-                .style("stroke", "#FFFFFF");
-        });
-    childEnterTransition.append("rect")
-        .classed("background", true)
-        .style("fill", function(d) {
-            return color(d.parent.name);
-        });
-    childEnterTransition.append('foreignObject')
-        .attr("class", "foreignObject")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", function(d) {
-            return Math.max(0.01, d.dy);
-        })
-        .append("xhtml:body")
-        .attr("class", "labelbody")
-        .append("div")
-        .attr("class", "label")
-        .text(function(d) {
-            return d.name;
+        var parents = nodes.filter(function(d) {
+            return d.children;
         });
 
+        // create parent cells
+        var parentCells = chart.selectAll("g.cell.parent")
+            .data(parents, function(d) {
+                return "p-" + d.id;
+            });
+        var parentEnterTransition = parentCells.enter()
+            .append("g")
+            .attr("class", "cell parent")
+            .on("click", function(d) {
+                zoom(d);
+            })
+            .append("svg")
+            .attr("class", "clip")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", headerHeight);
+        parentEnterTransition.append("rect")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", headerHeight)
+            .style("fill", headerColor);
+        parentEnterTransition.append('text')
+            .attr("class", "label")
+            .attr("transform", "translate(3, 13)")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", headerHeight)
+            .text(function(d) {
+                return d.name;
+            });
+        // update transition
+        var parentUpdateTransition = parentCells.transition().duration(transitionDuration);
+        parentUpdateTransition.select(".cell")
+            .attr("transform", function(d) {
+                return "translate(" + d.dx + "," + d.y + ")";
+            });
+        parentUpdateTransition.select("rect")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", headerHeight)
+            .style("fill", headerColor);
+        parentUpdateTransition.select(".label")
+            .attr("transform", "translate(3, 13)")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", headerHeight)
+            .text(function(d) {
+                return d.name;
+            });
+        // remove transition
+        parentCells.exit()
+            .remove();
 
-    childEnterTransition.selectAll(".foreignObject")
-        .style("display", "none");
+        // create children cells
+        var childrenCells = chart.selectAll("g.cell.child")
+            .data(children, function(d) {
+                return "c-" + d.id;
+            });
+        // enter transition
+        var childEnterTransition = childrenCells.enter()
+            .append("g")
+            .attr("class", "cell child")
+            .on("click", function(d) {
+                zoom(node === d.parent ? root : d.parent);
+            })
+            .on("mouseover", function() {
+                this.parentNode.appendChild(this); // workaround for bringing elements to the front (ie z-index)
+                d3.select(this)
+                    .attr("filter", "url(#outerDropShadow)")
+                    .select(".background")
+                    .style("stroke", "#000000");
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .attr("filter", "")
+                    .select(".background")
+                    .style("stroke", "#FFFFFF");
+            })
+            .append("svg")
+            .attr("class", "clip")
+        childEnterTransition.append("rect")
+            .classed("background", true)
+            .style("fill", function(d) {
+                return color(d.parent.name);
+            });
+        childEnterTransition.append('text')
+            .attr("class", "label")
+            .attr('x', function(d) {
+                return d.dx / 2;
+            })
+            .attr('y', function(d) {
+                return d.dy / 2;
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .style("display", "none")
+            .text(function(d) {
+                return d.name;
+            });
+        // update transition
+        var childUpdateTransition = childrenCells.transition().duration(transitionDuration);
+        childUpdateTransition.select(".cell")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        childUpdateTransition.select("rect")
+            .attr("width", function(d) {
+                return Math.max(0.01, d.dx);
+            })
+            .attr("height", function(d) {
+                return d.dy;
+            })
+            .style("fill", function(d) {
+                return color(d.parent.name);
+            });
+        childUpdateTransition.select(".label")
+            .attr('x', function(d) {
+                return d.dx / 2;
+            })
+            .attr('y', function(d) {
+                return d.dy / 2;
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .style("display", "none")
+            .text(function(d) {
+                return d.name;
+            });
 
-    // update transition
-    var childUpdateTransition = childrenCells.transition().duration(transitionDuration);
-    childUpdateTransition.select(".cell")
-        .attr("transform", function(d) {
-            return "translate(" + d.x  + "," + d.y + ")";
-        });
-    childUpdateTransition.select("rect")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", function(d) {
-            return d.dy;
-        })
-        .style("fill", function(d) {
-            return color(d.parent.name);
-        });
-    childUpdateTransition.select(".foreignObject")
-        .attr("width", function(d) {
-            return Math.max(0.01, d.dx);
-        })
-        .attr("height", function(d) {
-            return Math.max(0.01, d.dy);
-        })
-        .select(".labelbody .label")
-        .text(function(d) {
-            return d.name;
-        });
-    // exit transition
-    childrenCells.exit()
-        .remove();
+        // exit transition
+        childrenCells.exit()
+            .remove();
 
-    d3.select("select").on("change", function() {
-        console.log("select zoom(node)");
-        treemap.value(this.value == "size" ? size : count)
-            .nodes(root);
+        d3.select("select").on("change", function() {
+            console.log("select zoom(node)");
+            treemap.value(this.value == "size" ? size : count)
+                .nodes(root);
+            zoom(node);
+        });
+
         zoom(node);
-    });
-
-    zoom(node);
+    };
 
 
     function size(d) {
@@ -238,8 +257,7 @@ HTMLWidgets.widget({
         return (ky * d.dy) / headerHeight;
     }
 
-
-    function getRGBComponents (color) {
+    function getRGBComponents(color) {
         var r = color.substring(1, 3);
         var g = color.substring(3, 5);
         var b = color.substring(5, 7);
@@ -251,7 +269,7 @@ HTMLWidgets.widget({
     }
 
 
-    function idealTextColor (bgColor) {
+    function idealTextColor(bgColor) {
         var nThreshold = 105;
         var components = getRGBComponents(bgColor);
         var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
@@ -261,11 +279,11 @@ HTMLWidgets.widget({
 
     function zoom(d) {
         treemap
-            .padding([headerHeight/(chartHeight/d.dy), 0, 0, 0])
+            .padding([headerHeight / (chartHeight / d.dy), 0, 0, 0])
             .nodes(d);
 
         // moving the next two lines above treemap layout messes up padding of zoom result
-        var kx = chartWidth  / d.dx;
+        var kx = chartWidth / d.dx;
         var ky = chartHeight / d.dy;
         var level = d;
 
@@ -273,67 +291,65 @@ HTMLWidgets.widget({
         yscale.domain([d.y, d.y + d.dy]);
 
         if (node != level) {
-            if (isIE) {
-                chart.selectAll(".cell.child .foreignObject .labelbody .label")
-                    .style("display", "none");
-            } else {
-                chart.selectAll(".cell.child .foreignObject")
-                    .style("display", "none");
-            }
+            chart.selectAll(".cell.child .label")
+                .style("display", "none");
         }
 
         var zoomTransition = chart.selectAll("g.cell").transition().duration(transitionDuration)
             .attr("transform", function(d) {
                 return "translate(" + xscale(d.x) + "," + yscale(d.y) + ")";
             })
+            .each("start", function() {
+                d3.select(this).select("label")
+                    .style("display", "none");
+            })
             .each("end", function(d, i) {
-                if (!i && (level !== self.root)) {
+                if (!i && (level !== root)) {
                     chart.selectAll(".cell.child")
                         .filter(function(d) {
-                            return d.parent === self.node; // only get the children for selected group
+                            return d.parent === node; // only get the children for selected group
                         })
-                        .select(".foreignObject .labelbody .label")
-                        .style("color", function(d) {
+                        .select(".label")
+                        .style("display", "")
+                        .style("fill", function(d) {
                             return idealTextColor(color(d.parent.name));
                         });
-
-                    if (isIE) {
-                        chart.selectAll(".cell.child")
-                            .filter(function(d) {
-                                return d.parent === self.node; // only get the children for selected group
-                            })
-                            .select(".foreignObject .labelbody .label")
-                            .style("display", "")
-                    } else {
-                        chart.selectAll(".cell.child")
-                            .filter(function(d) {
-                                return d.parent === self.node; // only get the children for selected group
-                            })
-                            .select(".foreignObject")
-                            .style("display", "")
-                    }
                 }
             });
 
-        zoomTransition.select(".foreignObject")
+        zoomTransition.select(".clip")
             .attr("width", function(d) {
-                return Math.max(0.01, kx * d.dx);
+                return Math.max(0.01, (kx * d.dx));
             })
             .attr("height", function(d) {
-                return d.children ? headerHeight: Math.max(0.01, ky * d.dy);
+                return d.children ? headerHeight : Math.max(0.01, (ky * d.dy));
+            });
+
+        zoomTransition.select(".label")
+            .attr("width", function(d) {
+                return Math.max(0.01, (kx * d.dx));
             })
-            .select(".labelbody .label")
+            .attr("height", function(d) {
+                return d.children ? headerHeight : Math.max(0.01, (ky * d.dy));
+            })
             .text(function(d) {
                 return d.name;
             });
 
-        // update the width/height of the rects
+        zoomTransition.select(".child .label")
+            .attr("x", function(d) {
+                return kx * d.dx / 2;
+            })
+            .attr("y", function(d) {
+                return ky * d.dy / 2;
+            });
+
         zoomTransition.select("rect")
             .attr("width", function(d) {
-                return Math.max(0.01, kx * d.dx);
+                return Math.max(0.01, (kx * d.dx));
             })
             .attr("height", function(d) {
-                return d.children ? headerHeight : Math.max(0.01, ky * d.dy);
+                return d.children ? headerHeight : Math.max(0.01, (ky * d.dy));
             })
             .style("fill", function(d) {
                 return d.children ? headerColor : color(d.parent.name);
