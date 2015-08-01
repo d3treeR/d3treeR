862afbb70608fc9a4cbe743e9babd2023f7a11c2
timelyportfolio <kent.russell@timelyportfolio.com>
2015-07-31 20:18:22 -0500

    look for legend before trying to extract in case legend not drawn of draw = F in treemap

--- R/extractors.R
+++ R/extractors.R
@@ -1,5 +1,6 @@
 #' Extract Legends from Treemap
 #'
+#' @importFrom grid grid.grep
 #' @importFrom gridSVG grid.export
 #' @importFrom XML saveXML xpathSApply
 
@@ -7,11 +8,15 @@ extract_legend <- function( ){
   # assume that the current grid is our treemap
   #  this should not be too far-fetched considering the treemap
   #  is redrawn with d3tree( treemap(...) )
-  suppressWarnings(
-    XML::xpathSApply(
-      gridSVG::grid.export(name=NULL)$svg
-      ,"*/*/*[local-name()='g' and starts-with(@id,'legend')]"
-      ,XML::saveXML
+
+  # let's first check to see if a legend was drawn by treemap
+  if(!is.null(grid::grid.grep(".*legend.*",viewports=T,grep=T,no.match=NULL))) {
+    suppressWarnings(
+      XML::xpathSApply(
+        gridSVG::grid.export(name=NULL)$svg
+        ,"*/*/*[local-name()='g' and starts-with(@id,'legend')]"
+        ,XML::saveXML
+      )
     )
-  )
+  }
 }
